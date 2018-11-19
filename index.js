c9910df4bbc3022d1632b06a7412606363b168c0
maxwellbenton <maxwellbenton@gmail.com>
2018-11-19 15:00:28 -0500

    added solution

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const testVar = {}
+function fetchAstronautInfo() {
+  fetch( 'http://api.open-notify.org/astros.json' )
+    .then( res => res.json() )
+    .then( json => {
+      updateAstronautDisplay( json )
+      updateInfoBar( json )
 
-function testFunc() {
-  return "hi"
+    } )
 }
+
+function createAstronautElement() {
+  let astroElement = document.createElement( 'img' )
+  astroElement.src = 'astronaut.png'
+  return astroElement
+}
+
+function updateAstronautDisplay( json ) {
+  for ( let n = 0; n < json[ "people" ].length; n++ ) {
+    let astronaut = createAstronautElement( json[ "people" ][ n ] )
+    document.querySelector( 'main' ).appendChild( astronaut )
+  }
+}
+
+function updateInfoBar( json ) {
+  let header = document.querySelector( 'header' )
+  header.innerHTML = `There are ${json["number"]} people currently in space`
+}
+
+document.addEventListener( "DOMContentLoaded", ( event ) => {
+  fetchAstronautInfo()
+} );
